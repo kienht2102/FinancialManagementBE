f1aa1a73f59e042118beddd5bd468d6dcd5bee90
Twilight0908 <nguyenquang00.it@gmail.com>
2023-12-18 08:37:01 +0700

    up model

---
+++ src/main/java/com/example/financial_management/models/Output.java
@@ -0,0 +1,25 @@
+package com.example.financial_management.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class Output {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(columnDefinition = "TEXT")
+    private String title;
+    private long money;
+    private LocalDateTime createAt;
+    @ManyToOne
+    private User user;
+}
